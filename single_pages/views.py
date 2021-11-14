1cf7668e8b6a2d76058a25eb1046d93bce047221
haremeat <dmswjd9341@naver.com>
2021-11-14 13:11:55 +0900

    대문페이지와 자기소개 페이지 생성

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(req):
+    return render(
+        req,
+        'single_pages/landing.html'
+    )
+
+
+def about_me(req):
+    return render(
+        req,
+        'single_pages/about_me.html'
+    )
