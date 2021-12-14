d3ae2cb0f0caa9c30c96cda5b307afc8c7dfb99c
haremeat <dmswjd9341@naver.com>
2021-12-14 15:30:48 +0900

    카테고리 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,11 @@ from .models import Post
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'  # pk값이 작은 순서대로 보여줘라
+
     # template_name = 'blog/post_list.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -14,6 +16,28 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
 
         return context
+
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
+
 
 class PostDetail(DetailView):
     model = Post
