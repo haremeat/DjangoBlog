dd9804dae56809062464d5ad962c5b2da7f83e06
haremeat <dmswjd9341@naver.com>
2021-12-16 16:13:25 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class PostList(ListView):
@@ -38,6 +38,20 @@ def category_page(request, slug):
         }
     )
 
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+        }
+    )
 
 class PostDetail(DetailView):
     model = Post
