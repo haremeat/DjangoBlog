5ccbd3e4dea04dc556d59e914e2b6300648abf0d
haremeat <dmswjd9341@naver.com>
2021-12-17 13:41:01 +0900

    post create test

--- blog/tests.py
+++ blog/tests.py
@@ -33,6 +33,16 @@ class TestView(TestCase):
             author=self.user_obama
         )
 
+    def test_create_post(self):
+        response = self.client.get('/blog/create_post/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Create Post - Blog', soup.title.text)
+        main_area = soup.find('div', id='main_area')
+        self.assertIn('Create New Post', main_area.text)
+
+
     def navbar_test(self, soup):
         # 1.4 내비게이션 바가 있다.
         navbar = soup.find('div', id='navbar')
